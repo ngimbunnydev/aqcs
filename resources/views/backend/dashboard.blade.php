0e51f6a5e1dc15585a2ffc5281c54e4b63be8a0c
ngimbunnydev <ngimbunny@gmail.com>
2021-12-11 13:09:17 +0700

    Update dashboard.blade.php

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,143 +1,19 @@
 @extends('backend.layout')
 @section('header_import')
-<style>
-    #net-worth {
-        background: #2b7dbc;
-        color: #fff;
-        border-radius: 4px;
-        padding: 8px;
-    }
-    #new-worth-num{
-      margin-top: 0px;
-      border-bottom: 1px solid #fff;
-      padding-bottom: 15px;
-      font-size: 17px;
-      margin-bottom: 65px;
-    }
-    .summary-item{display:flex;line-height: 2;}
-    .summary-item-text{flex: 0 0 45%;}
-    #cash-flow, #sales-echart, .dashboard-echart{width: auto;height: 250px;}
-    #expense-category-echart{width:auto;height:250px;}
-    #new-worth-text{margin-bottom: 55px;text-transform: capitalize;}
-    .summary-item-num{text-transform: capitalize;}
-    @media only screen and (max-width: 736px){
-        #cash-flow {
-            margin-top: 30px;
-        }
-    }
-  </style>
+
 @stop
 
 
 
 @section('footer_import')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.8.0/echarts.min.js"></script>
-<script>
-	var cashFlowEchart = echarts.init(document.getElementById('cash-flow'));
-	var option;
 
-option = {
-  title: {
-    text: 'Stacked Line'
-  },
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      name: 'Email',
-      type: 'line',
-      stack: 'Total',
-      data: [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      name: 'Union Ads',
-      type: 'line',
-      stack: 'Total',
-      data: [220, 182, 191, 234, 290, 330, 310]
-    },
-    {
-      name: 'Video Ads',
-      type: 'line',
-      stack: 'Total',
-      data: [150, 232, 201, 154, 190, 330, 410]
-    },
-    {
-      name: 'Direct',
-      type: 'line',
-      stack: 'Total',
-      data: [320, 332, 301, 334, 390, 330, 320]
-    },
-    {
-      name: 'Search Engine',
-      type: 'line',
-      stack: 'Total',
-      data: [820, 932, 901, 934, 1290, 1330, 1320]
-    }
-  ]
-};
-	cashFlowEchart.setOption(option);
-
-	window.onresize = function() {
-      cashFlowEchart.resize();
-    };
-
-
-    function print() {
-        var contents = document.getElementById('app').innerHTML;
-        console.log(contents);
-        var frame1 = document.createElement('iframe');
-        frame1.name = "frame1";
-        frame1.style.position = "absolute";
-        frame1.style.top = "-1000000px";
-        document.body.appendChild(frame1);
-        var frameDoc = frame1.contentWindow ? frame1.contentWindow : frame1.contentDocument.document ? frame1.contentDocument.document : frame1.contentDocument;
-        frameDoc.document.open();
-        frameDoc.document.write(`<html><head><title>abc</title>`);
-        frameDoc.document.write('</head><body>');
-        frameDoc.document.write(contents);
-        
-        frameDoc.document.write('</body></html>');
-        frameDoc.document.close();
-        setTimeout(function () {
-            window.frames["frame1"].focus();
-            window.frames["frame1"].print();
-            document.body.removeChild(frame1);
-        }, 500);
-        return false;
-    }
-
-
-</script>
 @stop
 
 
 @section('app')
 <div id='app'>
   <h1> DASHBOARD</h1>
-	<div id="cash-flow">aaa</div>
+	
 </div>
 	
 	
